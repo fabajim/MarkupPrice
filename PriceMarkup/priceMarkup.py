d22147ecd4c74d54fc80792cc8d2a7b51bb114b5
Fabian Jimenez-Curiel <jimenfab@oregonstate.edu>
2024-08-13 09:41:30 -0700

    changed file names and translated output to english

--- PriceMarkup/priceMarkup.py
+++ PriceMarkup/priceMarkup.py
@@ -2,14 +2,14 @@ import sys
 from datetime import datetime
 
 
-class Busca_Precios:
+class Get_Markup:
 
     def __init__(self):
         # List to contain comments and date/time of entry
         self.comments = []
 
         # load comments from txt file and append to comments[]
-        with open('PriceMarkupSpanish/COMMENTS.txt', 'a+') as filehandle:
+        with open('PriceMarkup/COMMENTS.txt', 'a+') as filehandle:
             for line in filehandle:
                 # keeps from '\n' added to list
                 currentComment = line[:-1]
@@ -19,38 +19,37 @@ class Busca_Precios:
         """User inputs cost price and units
         User inputs the mark up percentage"""
         try:
-            cost = float(input("\nIngresar El Costo: "))
-            units = float(input("Ingresar Los Unidades o el Peso:  "))
-            markUp = float(input("Ingresar El Porcentaje de Aumento: "))
-
+            cost = float(input("\nEnter item's cost: "))
+            units = float(input("Enter items units or total weight:  "))
+            markUp = float(input("Enter markup percent: %"))
             # check if inputs are valid
             if cost >= 0 and units > 0 and markUp >= 0: 
                 # set sell price with the marked up amount
                 sell_price = (cost/units) * (markUp/100)
                 sell_price = sell_price + (cost/units)
-
                 # formats unit cost and sell price to two decimal places
                 formatted_cost = "{:.2f}".format(cost/units)
                 formatted_price = "{:.2f}".format(sell_price)
-                print("\n**************************************************"
-                      "\n* El Costo por unidad: $" , formatted_cost ,"                   *"
-                      "\n* El precio con el aumento de %" , markUp , " es: $",formatted_price, "*"
-                      "\n**************************************************")
+                print("\n**********************************************"
+                      "\n* Cost per unit: $", formatted_cost, ""
+                      "\n* Sell price with a", markUp, "% Markup: $", ""
+                      "", formatted_price, ""
+                      "\n**********************************************")
             else:
                 # invalid entry clause
-                print("\n**********Entrada Invalida************\n"
-                      "*El Costo no puede ser negativo      *\n"
-                      "*Los Unidades no puede ser 0 o menos *\n"
-                      "*El porcentaje no puede ser negaitvo *\n"
-                      "**************************************\n")  
+                print("\n**********Invalid Entry********\n"
+                      "*The cost cannot be negative\n"
+                      "*Units must be greater than 0\n"
+                      "*Percent cannot be negative\n"
+                      "**********************************\n")
         # control to only accept float inputs
         except ValueError:
-            print("\n***Solo usa numeros***"
-                  "\n**********************")
+            print("\n***Numbers Only!***"
+                  "\n*******************")
 
     def makeComment(self):
         """User can leave a note"""
-        add_note = input("\nIngrese un mensaje:\n")
+        add_note = input("\nEnter a message:\n")
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
@@ -73,60 +72,43 @@ class Busca_Precios:
         self.save_comments()
 
     def save_comments(self):
-        with open('PriceMarkupSpanish/COMMENTS.txt', 'w') as filehandle:
+        with open('PriceMarkup/COMMENTS.txt', 'w') as filehandle:
             for listitem in self.comments:
                 filehandle.write('%s\n' % listitem)
 
-    def print_groups(self):
-        # Displays linked group numbers
-        print("\n**********************"
-              "\n* 1 - Produce        *"
-              "\n* 2 - Grocery        *"
-              "\n* 3 - Taxed Grocery  *"
-              "\n* 4 - Carne          *"
-              "\n* 5 - Lecheria       *"
-              "\n* 6 - Panaderia      *"
-              "\n* 7 - Refrescos      *"
-              "\n* 8 - Licor          *"
-              "\n* 9 - Cerveza/Vino   *"
-              "\n* 11 - Health/Beauty *"
-              "\n**********************\n")
-
 
 if __name__ == '__main__':
-    precio = Busca_Precios()
-    print("*******BUSCAR EL PRECIO*******\n")
+    markup_val = Get_Markup()
+    print("*******Get Markup Price*******\n")
     menu = ""
     # menu used a loop control, 4 exits
-    while menu != '4':
+    while menu != '3':
         menu = input("\n*           MENU             *"
                      "\n******************************"
-                     "\n* 1 Para sacar un precio    *"
-                     "\n* 2 Para dejar un mensaje    *"
-                     "\n* 3 Para mostrar los codigos *"
-                     "\n* 4 Para salir               *"
+                     "\n* 1 To get price markup      *"
+                     "\n* 2 To leave a message       *"
+                     "\n* 3 To Exit                  *"
                      "\n******************************"
-                     "\n\nIngresa un numero: ")        
+                     "\n\nSelect and enter a number: ")        
         # 1 calls the mark up function
         if menu == '1':
-            while True:
-                precio.the_markup()
+            continueLoop = True
+            while continueLoop:
+                markup_val.the_markup()
 
                 # exit out of mark up loop with n or N
-                runAgain = input("\nContinuar? s/n ")
+                runAgain = input("\nContinue? y/n ")
                 if runAgain == 'n' or runAgain == 'N':
-                    break
+                    continueLoop = False
         elif menu == '2':
-            precio.makeComment()
-        elif menu == '3':
-            precio.print_groups()
+            markup_val.makeComment()
         elif menu == 'view notes':
-            precio.print_comments()
+            markup_val.print_comments()
         else:
             # invalid entry clause
-            if menu != '4':
-                print("\nENTRADA INVALIDA\n")
+            if menu != '3':
+                print("\nInvalid Entry\n")
     # Clears comments[]
-    precio.comments.clear()
+    markup_val.comments.clear()
     # end of program
     sys.exit()
